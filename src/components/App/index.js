1e63818aa71b02c9913bee2ad3fabbe9a8a0b886
AfamOkoh <afamokoh@gmail.com>
2022-06-11 22:48:30 +0100

    got a basic hardcoded version of BlogPost component working.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,36 @@
+import React from "react";
+import BlogPost from "../BlogPost";
+
+const examplePost = {
+  title: "My First Post",
+  author: "Chris Meah",
+  datePosted: "20/11/2019",
+  content: `
+    A structure used in most apps and games.
+    It's a way to keep doing the same.
+    While a condition is true,
+    Or for one to twenty-two.
+    If endless, for errors we blame
+    ..........
+    Loop `,
+  imageSrc:
+    "https://images.pexels.com/photos/1181472/pexels-photo-1181472.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  imageAlt: "A couple of coders.",
+};
+
 function App() {
-  return null;
+  return (
+    <main>
+      <BlogPost
+        title={examplePost.title}
+        author={examplePost.author}
+        datePosted={examplePost.datePosted}
+        content={examplePost.content}
+        imageSrc={examplePost.imageSrc}
+        imageAlt={examplePost.imageAlt}></BlogPost>
+      ;
+    </main>
+  );
 }
 
 export default App;
--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -1,5 +1,15 @@
-function BlogPost() {
-  return null;
+import React from "react";
+
+function BlogPost({ title, author, datePosted, content, imageSrc, imageAlt }) {
+  return (
+    <>
+      <h1>{title}</h1>
+      <h2>by {author}</h2>
+      <h4>{datePosted}</h4>
+      <img src={imageSrc} alt={imageAlt}></img>
+      <article>{content}</article>
+    </>
+  );
 }
 
 export default BlogPost;
